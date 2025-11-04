0641f0703b3d9f0f3d38539dc51402f24fd5554a
Ricken Diniz <87033966+ricken-diniz@users.noreply.github.com>
2025-11-04 18:40:30 -0300

    feat: Add model testing functionality in verify_acc.py

---
+++ FederatedCentral/nn/verify_acc.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from modelNet import Net
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    
+if __name__ == '__main__':
+
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('./data', download=True, train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=64, shuffle=True, **{})
+
+    model = Net()
+    model.load_state_dict(torch.load('federated_model.pt'))
+
+    test(model=model, device='cpu',test_loader=test_loader)
